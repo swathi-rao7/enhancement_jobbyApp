982b169aa15c93406c371cbf47e08df70ed0b3ea
Your Name <your-email@example.com>
2024-07-20 14:18:47 +0000

    Enhancement of JobbyApp

---
+++ src/components/FiltersGroup/index.js
@@ -0,0 +1,161 @@
+import {BsSearch} from 'react-icons/bs'
+
+// eslint-disable-next-line import/extensions
+import ProfileDetails from '../ProfileDetails'
+import './index.css'
+
+const FiltersGroup = props => {
+  const onChangeSearchInput = event => {
+    const {changeSearchInput} = props
+    changeSearchInput(event)
+  }
+
+  const onEnterSearchInput = event => {
+    const {getJobs} = props
+    if (event.key === 'Enter') {
+      getJobs()
+    }
+  }
+
+  const onLocationChange = event => {
+    const {changeLocation} = props
+    changeLocation(event.target.value)
+  }
+
+  const renderSearchInput = () => {
+    const {getJobs, searchInput} = props
+    return (
+      <div className="search-input-container">
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search"
+          value={searchInput}
+          onChange={onChangeSearchInput}
+          onKeyDown={onEnterSearchInput}
+        />
+        <button
+          type="button"
+          id="searchButton"
+          className="search-button-container"
+          onClick={getJobs}
+        >
+          -<BsSearch className="search-icon" />
+        </button>
+      </div>
+    )
+  }
+
+  const renderTypeOfEmployment = () => {
+    const {employmentTypesList} = props
+    return (
+      <div className="employment-type-container">
+        <h1 className="employment-type-heading">Type of Employment</h1>
+        <ul className="employee-type-list-container">
+          {employmentTypesList.map(eachEmployeeType => {
+            const {changeEmployeeList} = props
+            const onSelectEmployeeType = event => {
+              changeEmployeeList(event.target.value)
+            }
+            return (
+              <li
+                className="employee-item"
+                key={eachEmployeeType.employmentTypeId}
+              >
+                <input
+                  type="checkbox"
+                  id={eachEmployeeType.employmentTypeId}
+                  className="check-input"
+                  value={eachEmployeeType.employmentTypeId}
+                  onChange={onSelectEmployeeType}
+                />
+                <label
+                  htmlFor={eachEmployeeType.employmentTypeId}
+                  className="check-label"
+                >
+                  {eachEmployeeType.label}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+
+  const renderSalaryRange = () => {
+    const {salaryRangesList} = props
+    return (
+      <div className="container">
+        <h1 className="heading">Salary Range</h1>
+        <ul className="list-container">
+          {salaryRangesList.map(eachSalary => {
+            const {changeSalary} = props
+            const onClickSalary = () => {
+              changeSalary(eachSalary.salaryRangeId)
+            }
+            return (
+              <li
+                className="item"
+                key={eachSalary.salaryRangeId}
+                onClick={onClickSalary}
+              >
+                <input
+                  type="radio"
+                  id={eachSalary.salaryRangeId}
+                  name="salary"
+                  className="check-input"
+                />
+                <label
+                  htmlFor={eachSalary.salaryRangeId}
+                  className="check-label"
+                >
+                  {eachSalary.label}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+
+  const renderLocations = () => {
+    const {locations} = props
+    return (
+      <div className="container">
+        <h1 className="heading">Location</h1>
+        <ul className="list-container">
+          {locations.map(location => (
+            <li className="item" key={location.id}>
+              <input
+                type="checkbox"
+                id={location.id}
+                className="check-input"
+                value={location.id}
+                onChange={onLocationChange}
+              />
+              <label htmlFor={location.id} className="check-label">
+                {location.label}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  return (
+    <div className="filters-group-container">
+      {renderSearchInput()}
+      <ProfileDetails />
+      <hr className="horizontal-line" />
+      {renderTypeOfEmployment()}
+      <hr className="horizontal-line" />
+      {renderSalaryRange()}
+      <hr className="horizontal-line" />
+      {renderLocations()}
+    </div>
+  )
+}
+export default FiltersGroup
